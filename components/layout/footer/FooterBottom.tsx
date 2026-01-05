58226e9587c2d8554878c152468a0e5f6f7f3e83
Salekin Imran <abir045@gmail.com>
2026-01-05 15:34:02 +0600

    updated line height of copyright text in footer for desktop

--- components/layout/footer/FooterBottom.tsx
+++ components/layout/footer/FooterBottom.tsx
@@ -8,7 +8,7 @@ interface FooterBottomProps {
 export default function FooterBottom({ copyright, links }: FooterBottomProps) {
   return (
     <div className="w-full flex lg:flex-row lg:justify-between lg:items-start pt-16 gap-2.5 border-t border-t-white/16 flex-col">
-      <p className="text-base font-normal grow shrink-0 leading-[1.37] text-pulse-pink-50">
+      <p className="text-base font-normal grow shrink-0 leading-[1.37] xl:leading-[100%] text-pulse-pink-50">
         {copyright}
       </p>
       <div className="w-full flex flex-row grow flex-wrap lg:justify-end gap-x-4 text-pulse-pink-300 gap-y-2 text-sm font-normal leading-normal">
