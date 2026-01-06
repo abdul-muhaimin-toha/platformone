901fae65cf7eb179b59b059b804b8b1e65860ee1
Salekin Imran <abir045@gmail.com>
2026-01-06 16:17:44 +0600

    updated marker color in ol and ul in blogcontent in insights/details

--- components/sections/insights/BlogsContent.tsx
+++ components/sections/insights/BlogsContent.tsx
@@ -1,43 +1,39 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 function BlogsContent() {
   return (
     <div className="w-full block">
       <p className="text-xl leading-normal font-normal text-black my-8">
-        Feedback is the heartbeat of any customer experience strategy. It&apos;s the
-        unfiltered truth about how people perceive your brand, your products,
-        and your service. Yet, in many organizations, feedback is collected,
-        stored in a dashboard, and then left to gather digital dust. The
-        opportunity is lost—not because the feedback lacks value, but because
-        there&apos;s no clear path from insight to action.
+        Feedback is the heartbeat of any customer experience strategy. It&apos;s
+        the unfiltered truth about how people perceive your brand, your
+        products, and your service. Yet, in many organizations, feedback is
+        collected, stored in a dashboard, and then left to gather digital dust.
+        The opportunity is lost—not because the feedback lacks value, but
+        because there&apos;s no clear path from insight to action.
       </p>
       <p className="text-xl leading-normal font-normal text-black my-8">
-        The real challenge is not gathering feedback; it&apos;s knowing what to do
-        with it. Actionable insights don&apos;t just happen automatically. In this
-        article, we&apos;ll explore how organizations can move from simply listening
-        to truly acting-and in doing so, drive measurable growth.
+        The real challenge is not gathering feedback; it&apos;s knowing what to
+        do with it. Actionable insights don&apos;t just happen automatically. In
+        this article, we&apos;ll explore how organizations can move from simply
+        listening to truly acting-and in doing so, drive measurable growth.
       </p>
-      <ol className="text-xl leading-[1.3] text-black font-normal mt-8 mb-16 list-decimal flex flex-col gap-3 ml-6">
+      <ol className="text-xl marker:text-pulse-pink-600 leading-[1.3] text-black font-normal mt-8 mb-16 list-decimal flex flex-col gap-3 ml-6">
         <li>They&apos;re the result of asking the right questions.</li>
         <li>Connecting data from multiple touchpoints.</li>
-        <li>
-          Prioritizing improvements, and then executing with intention.
-        </li>
+        <li>Prioritizing improvements, and then executing with intention.</li>
       </ol>
       <h3 className="text-[32px] leading-tight font-semibold text-black mt-16 mb-8">
         Start with the Right Questions
       </h3>
       <p className="text-xl leading-normal font-normal text-black my-8">
-        The real challenge is not gathering feedback; it&apos;s knowing what to do
-        with it. Actionable insights don&apos;t just happen automatically. In this
-        article, we&apos;ll explore how organizations can move from simply listening
-        to truly acting-and in doing so, drive measurable growth.
+        The real challenge is not gathering feedback; it&apos;s knowing what to
+        do with it. Actionable insights don&apos;t just happen automatically. In
+        this article, we&apos;ll explore how organizations can move from simply
+        listening to truly acting-and in doing so, drive measurable growth.
       </p>
-      <ul className="text-xl leading-[1.3] text-black font-normal mt-8 mb-16 list-disc flex flex-col gap-3 ml-6">
+      <ul className="text-xl marker:text-pulse-pink-600 leading-[1.3] text-black font-normal mt-8 mb-16 list-disc flex flex-col gap-3 ml-6">
         <li>Every impactful insight starts with a purposeful question.</li>
-        <li>
-          Too often, organisations default to generic survey templates.
-        </li>
+        <li>Too often, organisations default to generic survey templates.</li>
         <li>You need to go deeper with open-ended prompts.</li>
       </ul>
       <h3 className="text-[32px] leading-tight font-semibold text-black mt-16 mb-8">
@@ -53,8 +49,8 @@ function BlogsContent() {
       <blockquote className="w-full bg-pulse-pink-50 flex flex-col p-6 gap-6 mt-8 mb-16">
         <p className="text-2xl font-normal leading-[1.33] text-mulberry-900">
           “Before Platform One, our feedback process felt like shouting into the
-          void. Now, we can see exactly where we&apos;re improving and where to focus
-          next.”
+          void. Now, we can see exactly where we&apos;re improving and where to
+          focus next.”
         </p>
         <p className="text-base font-normal leading-[1.37] text-[#6C6C6C]">
           Ray Poynter, Chief Research Officer at Platform One
@@ -64,10 +60,10 @@ function BlogsContent() {
         Prioritise What Matters
       </h3>
       <p className="text-xl leading-normal font-normal text-black my-8">
-        Once you&apos;ve consolidated your feedback, it&apos;s tempting to address
-        everything at once. But spreading your efforts too thin often leads to
-        shallow fixes that don&apos;t move the needle. Instead, prioritise issues
-        based on their potential business impact.
+        Once you&apos;ve consolidated your feedback, it&apos;s tempting to
+        address everything at once. But spreading your efforts too thin often
+        leads to shallow fixes that don&apos;t move the needle. Instead,
+        prioritise issues based on their potential business impact.
       </p>
       <Image
         src="/insights/top-card.png"
@@ -80,10 +76,10 @@ function BlogsContent() {
         Close the Loop
       </h3>
       <p className="text-xl leading-normal font-normal text-black my-8">
-        Once you&apos;ve consolidated your feedback, it&apos;s tempting to address
-        everything at once. But spreading your efforts too thin often leads to
-        shallow fixes that don&apos;t move the needle. Instead, prioritise issues
-        based on their potential business impact.
+        Once you&apos;ve consolidated your feedback, it&apos;s tempting to
+        address everything at once. But spreading your efforts too thin often
+        leads to shallow fixes that don&apos;t move the needle. Instead,
+        prioritise issues based on their potential business impact.
       </p>
       <div className="flex flex-row md:grid md:grid-cols-2 xl:grid-cols-3 items-center gap-6 overflow-auto pb-4 mt-8 grow-0">
         <div className="flex md:w-full flex-col p-6 gap-4 rounded-2xl border border-border min-w-[290px] md:min-w-auto">
@@ -115,10 +111,10 @@ function BlogsContent() {
         Final Thought
       </h3>
       <p className="text-xl leading-normal font-normal text-black my-8">
-        Once you&apos;ve consolidated your feedback, it&apos;s tempting to address
-        everything at once. But spreading your efforts too thin often leads to
-        shallow fixes that don&apos;t move the needle. Instead, prioritise issues
-        based on their potential business impact.
+        Once you&apos;ve consolidated your feedback, it&apos;s tempting to
+        address everything at once. But spreading your efforts too thin often
+        leads to shallow fixes that don&apos;t move the needle. Instead,
+        prioritise issues based on their potential business impact.
       </p>
     </div>
   );
