475f8840536ee82c0d6a5f5bd5eb5dabb07a8e92
Salekin Imran <abir045@gmail.com>
2026-01-07 12:57:00 +0600

    updated spelling to locatins

--- components/sections/contactUs/locationCards/LocationHeader.tsx
+++ components/sections/contactUs/locationCards/LocationHeader.tsx
@@ -5,7 +5,7 @@ function LocationHeader() {
       className="flex items-left flex-col justify-center lg:flex-row text-left gap-6 md:max-w-[600px] lg:max-w-max lg:items-center"
     >
       <h2 className="text-[56px] leading-[1.28] lg:w-1/2 font-bold text-black">
-        Location
+        Locations
       </h2>
       <p className="text-xl font-normal leading-[1.33] lg:w-1/2 text-black">
         We operate across key locations worldwide, with client-facing teams and
